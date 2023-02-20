23a946245bb2fac9618f92a0e262ad99b870b267
Johannes Bosecker <j.bosecker.dev@icloud.com>
2023-02-20 19:13:01 +0100

    Better logging.

--- eltako/device.py
+++ eltako/device.py
@@ -38,7 +38,7 @@ class EltakoEntity(Entity):
         except ParseError:
             pass
         else:
-            LOGGER.debug("We got a Eltako wrapped RPS message with address %s. Device address is %s.", msg.address, self.dev_id)
+            LOGGER.debug("We got a Eltako wrapped RPS message with address %s. Device address is %s.", msg.address.hex("-"), self.dev_id.hex("-"))
             if msg.address == self.dev_id:
                 self.value_changed(msg)
             return
@@ -49,7 +49,7 @@ class EltakoEntity(Entity):
         except ParseError:
             pass
         else:
-            LOGGER.debug("We got a Eltako wrapped 4BS message with address %s. Device address is %s.", msg.address, self.dev_id)
+            LOGGER.debug("We got a Eltako wrapped 4BS message with address %s. Device address is %s.", msg.address.hex("-"), self.dev_id.hex("-"))
             if msg.address == self.dev_id:
                 self.value_changed(msg)
             return
@@ -60,7 +60,7 @@ class EltakoEntity(Entity):
         except ParseError as e:
             pass
         else:
-            LOGGER.debug("We got a RPS message with address %s. Device address is %s.", msg.address, self.dev_id)
+            LOGGER.debug("We got a RPS message with address %s. Device address is %s.", msg.address.hex("-"), self.dev_id.hex("-"))
             if msg.address == self.dev_id:
                 self.value_changed(msg)
             return
@@ -71,7 +71,7 @@ class EltakoEntity(Entity):
         except ParseError:
             pass
         else:
-            LOGGER.debug("We got a 4BS message with address %s. Device address is %s.", msg.address, self.dev_id)
+            LOGGER.debug("We got a 4BS message with address %s. Device address is %s.", msg.address.hex("-"), self.dev_id.hex("-"))
             if msg.address == self.dev_id:
                 self.value_changed(msg)
             return
