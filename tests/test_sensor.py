fab14ddcc4da7ab68ce8a3c276a2899086773d59
Philipp Grimm <grimm.philipp@gmail.com>
2023-05-19 22:10:54 +0200

    added weatherstation sensor

---
+++ tests/test_sensor.py
@@ -0,0 +1,56 @@
+import unittest
+from custom_components.eltako.sensor import *
+from mocks import HassMock
+from unittest import mock
+from homeassistant.helpers.entity import Entity
+from custom_components.eltako.binary_sensor import EltakoBinarySensor
+from eltakobus import *
+
+# mock update of Home Assistant
+Entity.schedule_update_ha_state = mock.Mock(return_value=None)
+# EltakoBinarySensor.hass.bus.fire is mocked by class HassMock
+
+
+class TestSensor(unittest.TestCase):
+
+    def create_weatherstation_sensor(self, description: EltakoSensorEntityDescription) -> EltakoWeatherStation:
+        gateway = None
+        dev_id = AddressExpression.parse("51-E8-00-01")
+        dev_name = "dev name"
+        dev_eep = EEP.find("A5-13-01")
+        ews = EltakoWeatherStation(gateway, dev_id, dev_name, dev_eep, description)
+
+        return ews
+
+    def test_weatherstation_sensor(self):
+        ews = self.create_weatherstation_sensor(SENSOR_DESC_WEATHER_STATION_ILLUMINANCE_DAWN)
+        
+        msg = Regular4BSMessage(address=b'\x05\x1e\x83\x15', status=b'\x00', data=b'\x0f\x7d\x07\x1a', outgoing=False)
+        
+        ews.entity_description = SENSOR_DESC_WEATHER_STATION_ILLUMINANCE_DAWN
+        ews.value_changed(msg)
+        self.assertEqual(ews.native_value, 58.76470588235294)
+
+        ews.entity_description = SENSOR_DESC_WEATHER_STATION_TEMPERATURE
+        ews.value_changed(msg)
+        self.assertEqual(ews.native_value, 18.823529411764703)
+
+        ews.entity_description = SENSOR_DESC_WEATHER_STATION_WIND_SPEED
+        ews.value_changed(msg)
+        self.assertEqual(ews.native_value, 1.9215686274509804)
+
+        ews.entity_description = SENSOR_DESC_WEATHER_STATION_ILLUMINANCE_EAST
+        ews.value_changed(msg)
+        self.assertEqual(ews.native_value, 1.9215686274509804)
+
+        ews.entity_description = SENSOR_DESC_WEATHER_STATION_ILLUMINANCE_CENTRAL
+        ews.value_changed(msg)
+        self.assertEqual(ews.native_value, 1.9215686274509804)
+
+        ews.entity_description = SENSOR_DESC_WEATHER_STATION_ILLUMINANCE_WEST
+        ews.value_changed(msg)
+        self.assertEqual(ews.native_value, 1.9215686274509804)
+
+        ews.entity_description = SENSOR_DESC_WEATHER_STATION_RAIN
+        ews.value_changed(msg)
+        self.assertEqual(ews.native_value, 1)
