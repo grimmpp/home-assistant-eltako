2d78a60042e1e6a7971d9ea5b95f74b8e110b210
Philipp Grimm <grimm.philipp@gmail.com>
2023-11-09 13:52:50 +0100

    schema for gateway debug

--- custom_components/eltako/__init__.py
+++ custom_components/eltako/__init__.py
@@ -91,8 +91,6 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> b
         hass.async_create_task(
             hass.config_entries.async_forward_entry_setup(config_entry, platform)
         )
-        
-    LOGGER.debug(f"hass: {hass}")
 
     return True
 
