c5d9774c0f3aeb3d395d6c156735dd23a9a12208
Philipp Grimm <grimm.philipp@gmail.com>
2023-12-07 00:37:35 +0100

    disabled teach-in buttons for cable connected gateways

--- custom_components/eltako/gateway.py
+++ custom_components/eltako/gateway.py
@@ -183,7 +183,7 @@ class EltakoGatewayFam14 (EltakoGateway):
     def validate_dev_id(self, dev_id: AddressExpression, device_name: str = "") -> bool:
         result = compare_enocean_ids(b'\x00\x00\x00\x00', dev_id[0], pos=2)
         if not result:
-            LOGGER.warn(f"{device_name} ({dev_id}): Maybe wrong device id configured!")
+            LOGGER.warn(f"{device_name} ({dev_id}): Maybe have wrong device id configured!")
         return result
 
 class EltakoGatewayFgw14Usb (EltakoGatewayFam14):
@@ -218,14 +218,14 @@ class EltakoGatewayFamUsb (EltakoGateway, Entity):
     def validate_sender_id(self, sender_id: AddressExpression, device_name: str = "") -> bool:
         result = compare_enocean_ids(self.base_id[0], sender_id[0])
         if not result:
-            LOGGER.warn(f"{device_name} ({sender_id}): Maybe wrong sender id configured!")
+            LOGGER.warn(f"{device_name} ({sender_id}): Maybe have wrong sender id configured!")
         return result
 
     
     def validate_dev_id(self, dev_id: AddressExpression, device_name: str = "") -> bool:
         result = 0xFF == dev_id[0][0]
         if not result:
-            LOGGER.warn(f"{device_name} ({dev_id}): Maybe wrong device id configured!")
+            LOGGER.warn(f"{device_name} ({dev_id}): Maybe have wrong device id configured!")
         return result
     
     
