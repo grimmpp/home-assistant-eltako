79753c6f987a3e59cc64db4e0d39d2ed65867e69
Philipp Grimm <grimm.philipp@gmail.com>
2024-02-14 00:14:50 +0100

    debug last message received

--- custom_components/eltako/device.py
+++ custom_components/eltako/device.py
@@ -120,6 +120,7 @@ class EltakoEntity(Entity):
 
             elif hasattr(self, '_attr_is_on'):
                 self._attr_is_on = 'on' == latest_state.state
+                self._attr_state = latest_state.state
 
             elif attributs.get('state_class', None) == 'measurement':
                 if '.' in  latest_state.state:
