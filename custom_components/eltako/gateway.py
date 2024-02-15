2e51b991c0043f8f953edd1678220b5068970f1f
Philipp Grimm <grimm.philipp@gmail.com>
2024-02-16 00:21:15 +0100

    cleaned up

--- custom_components/eltako/gateway.py
+++ custom_components/eltako/gateway.py
@@ -133,7 +133,7 @@ class EnOceanGateway:
         device_registry.async_get_or_create(
             config_entry_id=self.config_entry_id,
             identifiers={(DOMAIN, self.serial_path)},
-            connections={(CONF_MAC, config_helpers.format_address(self.base_id))},
+            # connections={(CONF_MAC, config_helpers.format_address(self.base_id))},
             manufacturer=MANUFACTURER,
             name= self.dev_name,
             model=self.model,
