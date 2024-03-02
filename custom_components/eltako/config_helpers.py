b5d5186eb8eb176e232b41a4c1fd847d16d2b849
Philipp Grimm <grimm.philipp@gmail.com>
2024-03-02 23:09:52 +0100

    fixed error messages when reloading gateway

--- custom_components/eltako/config_helpers.py
+++ custom_components/eltako/config_helpers.py
@@ -224,13 +224,18 @@ def filter_for_new_entities(registry: EntityRegistry, entities: list[Entity]) ->
         LOGGER.info(f"[config_helper] check if entity exists e_id: {e.entity_id}, e.unique_id: {e.unique_id}")
         re1:RegistryEntry = registry.async_get(e.entity_id)
         if re1 is not None:
-            new_entities.append(e)
             LOGGER.info(f"[config_helper] e_id: {e.entity_id} alread exists")
+        else:
+            LOGGER.info(f"[config_helper] found e_id: {e.entity_id}")
+
         re2:RegistryEntry = registry.async_get(e.unique_id)
         if re2 is not None:
-            new_entities.append(e)
             LOGGER.info(f"[config_helper] e_unique_id: {e.unique_id} alread exists")
+        else:
+            LOGGER.info(f"[config_helper] found e_unique_id: {e.unique_id}")
 
         if re1 is None and re2 is None:
             LOGGER.info(f"[config_helper] nothing found for e_id: {e.entity_id} and e_unique_id: {e.unique_id}")
+            new_entities.append(e)
+
     return new_entities
