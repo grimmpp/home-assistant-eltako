75b426510850f5375465b53fac1c40f9fcbc3cbb
Philipp Grimm <grimm.philipp@gmail.com>
2024-05-13 11:31:27 +0200

    test fire_last_message_received

--- custom_components/eltako/gateway.py
+++ custom_components/eltako/gateway.py
@@ -123,7 +123,7 @@ class EnOceanGateway:
 
     def _init_bus(self):
         self._received_message_count = 0
-        self._fire_received_message_count_event()
+        # self._fire_received_message_count_event()
         if GatewayDeviceType.is_esp2_gateway(self.dev_type):
             self._bus = RS485SerialInterfaceV2(self.serial_path, baud_rate=self.baud_rate, callback=self._callback_receive_message_from_serial_bus)
         else:
@@ -297,8 +297,8 @@ class EnOceanGateway:
 
         if type(message) not in [EltakoPoll]:
             LOGGER.debug("[Gateway] [Id: %d] Received message: %s", self.dev_id, message)
-            self._fire_last_message_received_event()
-            self._fire_received_message_count_event()
+            # self._fire_last_message_received_event()
+            # self._fire_received_message_count_event()
             if isinstance(message, ESP2Message):
                 event_id = config_helpers.get_bus_event_type(self.base_id, SIGNAL_RECEIVE_MESSAGE)
                 dispatcher_send(self.hass, event_id, message)
