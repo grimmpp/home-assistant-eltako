81df433c6bdc50c07916ddcd8a221a3522f15273
Philipp Grimm <grimm.philipp@gmail.com>
2024-10-07 11:22:23 +0200

    test tcp server

--- custom_components/eltako/eltako_integration_init.py
+++ custom_components/eltako/eltako_integration_init.py
@@ -148,10 +148,7 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> b
     set_gateway_to_hass(hass, usb_gateway)
 
     hass.data[DATA_ELTAKO][DATA_ENTITIES] = {}
-    for platform in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(config_entry, platform)
-        )
+    await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
 
     VIRTUAL_TCP_SERVER.start_tcp_server()
 
