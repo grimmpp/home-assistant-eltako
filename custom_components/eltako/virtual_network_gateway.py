96add9cbde08b8fc0b3106f927e5005adc2932c6
Philipp Grimm <grimm.philipp@gmail.com>
2024-10-10 00:24:27 +0200

    test tcp server

--- custom_components/eltako/virtual_network_gateway.py
+++ custom_components/eltako/virtual_network_gateway.py
@@ -49,7 +49,7 @@ class VirtualNetworkGateway:
     def get_service_info(self, ip_address:str):
         info = ServiceInfo(
             "_bsc-sc-socket._tcp.local.",
-            "_virtual-network-gateway-adapter._bsc-sc-socket._tcp.local.",
+            "Virtual-Network-Gateway-Adapter._bsc-sc-socket._tcp.local.",
             addresses = [self.convert_ip_to_bytes(ip_address)],
             port=self.port,
         )
