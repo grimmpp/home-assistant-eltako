c9e3d27df72e63f3bf1dae09f9641181e442871d
Philipp Grimm <grimm.philipp@gmail.com>
2024-10-10 23:25:45 +0200

    try to make base_id optional

--- custom_components/eltako/gateway.py
+++ custom_components/eltako/gateway.py
@@ -96,8 +96,8 @@ class EnOceanGateway:
     def query_for_base_id_and_version(self, connected):
         if connected:
             LOGGER.debug("[Gateway] [Id: %d] Query for base id and version info.", self.dev_id)
-            self.hass.async_add_job( self._bus.send_base_id_request )
-            self.hass.async_add_job( self._bus.send_version_request )
+            # self.hass.async_add_job( self._bus.send_base_id_request )
+            # self.hass.async_add_job( self._bus.send_version_request )
 
 
     def add_connection_state_changed_handler(self, handler):
