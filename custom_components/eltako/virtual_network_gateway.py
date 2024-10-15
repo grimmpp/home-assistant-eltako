679cdbcf5e91a2d4170501fab1730275f514d3a3
Philipp Grimm <grimm.philipp@gmail.com>
2024-10-15 23:40:53 +0200

    debugging tcp gateway problems

--- custom_components/eltako/virtual_network_gateway.py
+++ custom_components/eltako/virtual_network_gateway.py
@@ -98,7 +98,7 @@ class VirtualNetworkGateway(EnOceanGateway):
         for gw in self.sending_gateways:
             try:
                 ## send base id and put gateway type as well into it
-                gw_type_id:int = GatewayDeviceType.indexOf(gw.dev_type)
+                gw_type_id:int = GatewayDeviceType.indexOf(gw.dev_type) + 1
                 data:bytes = b'\x8b\x98' + gw.base_id[0] + gw_type_id.to_bytes(1, 'big') + b'\x00\x00\x00\x00'
                 LOGGER.debug(f"[{LOGGING_PREFIX_VIRT_GW}] Send gateway info {gw} (id: {gw.dev_id}, base id: {b2s(gw.base_id[0])}, type: {gw.dev_type} / {gw_type_id}) ")
                 conn.sendall( ESP2Message(bytes(data)).serialize() )
